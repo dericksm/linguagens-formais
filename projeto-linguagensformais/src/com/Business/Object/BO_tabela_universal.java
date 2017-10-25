d450c4c669b4e2dd78550b06214ccd881fc62593
comp9 <comp9@comp9_21.lab.unesc.net>
2017-10-25 20:56:23 -0200

    Adicionado classes bases

---
+++ projeto-linguagensformais/src/com/Business/Object/BO_tabela_universal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Object;
+
+/**
+ *
+ * @author comp9
+ */
+public class BO_tabela_universal {
+    
+    private String Inicial;
+    private String[] Final;
+    
+}
---
+++ projeto-linguagensformais/src/com/Model/Class/ModelTransicao.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Model.Class;
+
+/**
+ *
+ * @author comp9
+ */
+public class ModelTransicao {
+    
+    private String inicial;
+    private String transicao;
+    private String destino;
+
+    public ModelTransicao() {
+    }
+
+    public String getInicial() {
+        return inicial;
+    }
+
+    public void setInicial(String inicial) {
+        this.inicial = inicial;
+    }
+
+    public String getTransicao() {
+        return transicao;
+    }
+
+    public void setTransicao(String transicao) {
+        this.transicao = transicao;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+}
